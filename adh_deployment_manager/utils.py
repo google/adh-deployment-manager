ec23c54f961d7fb7f5d676d98a454388158d30c3
Andrei Markin <amarkin@google.com>
2021-10-01 15:02:48 +0300

    Add LICENSE information and CONTRIBUTING.md
    Change-Id: Ib920fb7d5666263907a2c1f9036e8f4fc3a94a96

--- adh_deployment_manager/utils.py
+++ adh_deployment_manager/utils.py
@@ -1,3 +1,18 @@
+#
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import logging
 import os
 from typing import Dict, Any, List, Optional, Union, NamedTuple
@@ -56,5 +71,6 @@ def get_file_content(relative_path: str, working_directory: str = None) -> str:
         working_directory = Path(working_directory)
     with open(os.path.join(working_directory, relative_path),
               "r") as sql_query:
-        query_txt = sql_query.read()
+        query_lines = sql_query.readlines()
+        query_txt = "".join(line for line in query_lines if not line.startswith("#"))
         return query_txt.strip()
